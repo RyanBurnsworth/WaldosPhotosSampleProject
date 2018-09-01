67cd20b5055e99df31a3ab103d1a07e175118195
Ryan <RyanBurnsworth@gmail.com>
2018-08-31 23:16:43 -0400

    Added GridLayout RecyclerView; Displaying images in grid

--- app/src/main/java/com/ryanburnsworth/waldosphotossample/view/PhotoGalleryActivity.java
+++ app/src/main/java/com/ryanburnsworth/waldosphotossample/view/PhotoGalleryActivity.java
@@ -4,10 +4,14 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.ryanburnsworth.waldosphotossample.R;
 import com.ryanburnsworth.waldosphotossample.viewmodel.PhotoGalleryViewModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PhotoGalleryActivity extends AppCompatActivity {
@@ -17,13 +21,18 @@ public class PhotoGalleryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        final PhotoGalleryAdapter adapter = new PhotoGalleryAdapter(getApplicationContext(), new ArrayList<String>());
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(), 3);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
         PhotoGalleryViewModel photoGalleryViewModel = ViewModelProviders.of(this).get(PhotoGalleryViewModel.class);
         photoGalleryViewModel.getPhotoUrlLiveDataList().observe(this, new android.arch.lifecycle.Observer<List<String>>() {
             @Override
             public void onChanged(@Nullable List<String> strings) {
-                for (String str : strings) {
-                    // TODO: pass strings to PhotoGalleryAdapter
-                }
+                adapter.addItems(strings);
             }
         });
     }
--- app/src/main/java/com/ryanburnsworth/waldosphotossample/view/PhotoGalleryAdapter.java
+++ app/src/main/java/com/ryanburnsworth/waldosphotossample/view/PhotoGalleryAdapter.java
@@ -1,5 +1,66 @@
 package com.ryanburnsworth.waldosphotossample.view;
 
-public class PhotoGalleryAdapter {
+import android.content.Context;
+import android.support.v4.widget.CircularProgressDrawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.ryanburnsworth.waldosphotossample.R;
+
+import java.util.List;
+
+public class PhotoGalleryAdapter extends RecyclerView.Adapter<PhotoGalleryAdapter.PhotoViewHolder> {
+    private List<String> photoUrls;
+    private Context context;
+
+    public PhotoGalleryAdapter(Context context, List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+        this.context = context;
+    }
+
+    @Override
+    public PhotoGalleryAdapter.PhotoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.photo_item, parent, false);
+        return new PhotoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PhotoGalleryAdapter.PhotoViewHolder holder, int position) {
+        CircularProgressDrawable circularProgressDrawable = new CircularProgressDrawable(context);
+        circularProgressDrawable.setStrokeWidth(5f);
+        circularProgressDrawable.setCenterRadius(30f);
+        circularProgressDrawable.start();
+        Glide.with(context)
+                .load(photoUrls.get(position))
+                .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.ALL))
+                .apply(new RequestOptions().placeholder(circularProgressDrawable))
+                .thumbnail(0.5f)
+                .into(holder.photo);
+    }
+
+    @Override
+    public int getItemCount() {
+        return photoUrls.size();
+    }
+
+    static class PhotoViewHolder extends RecyclerView.ViewHolder {
+        private ImageView photo;
+
+        public PhotoViewHolder(View itemView) {
+            super(itemView);
+            photo = itemView.findViewById(R.id.photo);
+        }
+    }
+
+    public void addItems(List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+        notifyDataSetChanged();
+    }
 }
